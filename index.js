4798cfc0c7b1c7b26b8f68cff30892b94920a0fd
Noelle Foster <uufarmgirl@gmail.com>
2018-07-18 16:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logShout(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHitToGrandma(string){
+  if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }
+  else if(string==="I love you, Grandma."){
+    return "I love you, too."
+  }
+  else {
+    return "I can't hear you!"
+  }
+  }
